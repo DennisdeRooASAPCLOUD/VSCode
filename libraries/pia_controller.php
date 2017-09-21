20b5ab27701cd864602a19b79d8de7035da15fd4
DennisdeRooAsapnet <dennis@asapnet.com>
2017-09-21 21:36:00 +0200

    Add files via upload

---
+++ libraries/pia_controller.php
@@ -0,0 +1,30 @@
+<?php
+	/* libraries/pia_controller.php
+	 *
+	 * Copyrsght (C) by Hugo Leisink <hugo@leisink.net>
+	 * This file is part of the Banshee PHP framework
+	 * http://www.banshee-php.org/
+	 */
+
+	abstract class pia_controller extends controller {
+		protected $pia = null;
+		protected $breadcrumbs = array(
+			"pia/casus"     => "PIA casussen",
+			"pia/pia/#"     => "Privacy Impact Assessment",
+			"pia/rapport/#" => "Rapportage");
+
+		protected function valid_pia_id($pia_id) {
+			if (valid_input($pia_id, VALIDATE_NUMBERS, VALIDATE_NONEMPTY) == false) {
+				$this->output->add_tag("result", "Geen PIA casus opgegeven.", array("url" => $this->settings->start_page));
+				return false;
+			} else if (($this->pia = $this->model->get_pia_case($pia_id)) == false) {
+				$this->output->add_tag("result", "Onbekende PIA casus.", array("url" => $this->settings->start_page));
+				return false;
+			}
+
+			$this->output->add_tag("pia", $this->pia["name"]);
+
+			return true;
+		}
+	}
+?>
