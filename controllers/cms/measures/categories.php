12571b1c27b7aa085887568e604eca93014234d3
DennisdeRooAsapnet <dennis@asapnet.com>
2017-09-21 21:32:16 +0200

    Add files via upload

---
+++ controllers/cms/measures/categories.php
@@ -0,0 +1,19 @@
+<?php
+	class cms_measures_categories_controller extends tablemanager_controller {
+		protected $name = "Measure category";
+		protected $pathinfo_offset = 3;
+		protected $back = "cms/measures";
+		protected $icon = "measure_categories.png";
+		protected $browsing = null;
+
+		public function execute() {
+			$this->output->add_css("includes/standard.css");
+
+			if (($standard = $this->model->get_iso_standard($_SESSION["iso_standard"])) != false) {
+				$this->output->add_tag("standard", $standard["name"]);
+			}
+
+			parent::execute();
+		}
+	}
+?>
